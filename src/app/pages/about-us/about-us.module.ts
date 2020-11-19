96e7a53f6eec179714b0fc5e91b97137b4c522ee
Shaheryar Khan <45919322+ShaheryarK@users.noreply.github.com>
2020-11-19 08:49:55 +0500

    added UI updates related to About US page

--- src/app/pages/about-us/about-us.module.ts
+++ src/app/pages/about-us/about-us.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [AboutUsComponent],
   imports: [
     CommonModule,
-    AboutUsRoutingModule
+    AboutUsRoutingModule,
+    FormsModule
   ]
 })
 export class AboutUsModule { }
